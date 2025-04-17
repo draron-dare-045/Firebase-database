f54b18eacb2d89f328df599dc531185206ee8c06
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-04-17 07:32:10 +0300

    hid some things

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3iiaQHUAjLBwbEot46o37R-hsr45paCA",
-  authDomain: "test-2de0c.firebaseapp.com",
-  databaseURL: "https://test-2de0c-default-rtdb.firebaseio.com",
-  projectId: "test-2de0c",
-  storageBucket: "test-2de0c.firebasestorage.app",
-  messagingSenderId: "997292452209",
-  appId: "1:997292452209:web:7cbf9df095fb73f55d6ed0",
-  measurementId: "G-571Z3365Y6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
-export const auth = getAuth(app); 
-export const provider = new GoogleAuthProvider(); 
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
